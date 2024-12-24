8a06e97be0fadef71144843e5cf3b860f5dd64c0
grazyfunk04 <kunalmehndi10@gmail.com>
2024-12-24 21:14:17 +0530

    commit

--- src/main/java/com/example/portfolio/tracker/WebConfig.java
+++ src/main/java/com/example/portfolio/tracker/WebConfig.java
@@ -10,10 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000") // Allow requests from the frontend (React, Angular, etc.)
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods (GET, POST, etc.)
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true); // Allow credentials (cookies, HTTP authentication)
+                .allowedOrigins("https://portfolio-frontend-alpha-five.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
 
